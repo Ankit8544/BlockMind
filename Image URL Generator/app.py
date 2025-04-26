4b5b5820b6ae4651e3b56fdc1a4a539c7a9b3403
Ankit8544 <ankitkumar875740@gmail.com>
2025-04-26 17:45:53 +0530

    Update

--- Image URL Generator/app.py
+++ Image URL Generator/app.py
@@ -43,4 +43,5 @@ def serve_uploaded_image(filename):
     return send_from_directory(os.path.abspath(app.config['UPLOAD_FOLDER']), filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ✅ auto detect Render port
+    app.run(host="0.0.0.0", port=port, debug=True)
