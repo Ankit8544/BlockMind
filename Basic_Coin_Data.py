8815ba8f7f7465f9b029ff3b3f00370d60ee81ab
Ankit8544 <ankitkumar875740@gmail.com>
2025-06-05 21:15:16 +0530

    Update

--- Basic_Coin_Data.py
+++ Basic_Coin_Data.py
@@ -8,7 +8,8 @@ COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
 lock = Lock()
 next_available_time = time.time()
 
-
+# Coins you want to fetch data for
+coin_ids = ["dogecoin", "shiba-inu", "pepe"]  # Replace with your list
 
 def fetch_coin_data(coin_id):
     global next_available_time
